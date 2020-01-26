1d91a6961f5167df24a7775f7394bc99895975de
William <liams_1998@hotmail.com>
2020-01-26 20:23:21 +0200

    Added player movement + rotation

---
+++ The-Big-One/Assets/Script/PlayerControls.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float PlayerSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.MakePlayerLookAtPointer();
+        this.MovePlayer();
+    }
+
+    void MakePlayerLookAtPointer()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 playerPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        Vector2 direction = new Vector2(mousePos.x, mousePos.y) - new Vector2(playerPos.x, playerPos.y);
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+    }
+
+
+    private Vector3 moveDirection = Vector3.zero;
+    void MovePlayer()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveDirection *= PlayerSpeed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+
+}
